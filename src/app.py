0430be6c611b3ea4697f87268097fcb066b6c779
zuletacarlos <carlosluiszuleta@yahoo.es>
2026-01-05 22:00:26 +0000

    error corregido finalizado

--- src/app.py
+++ src/app.py
@@ -1,6 +1,4 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
+
 import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -13,18 +11,22 @@ CORS(app)
 
 jackson_family = FamilyStructure("Jackson")
 
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
+
 
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/members', methods=['GET'])
 def get_all_members():
     members = jackson_family.get_all_members()
     return jsonify(members), 200
+
 
 @app.route('/members/<int:member_id>', methods=['GET'])
 def get_single_member(member_id):
@@ -34,25 +36,28 @@ def get_single_member(member_id):
     else:
         return jsonify({"msg": "Miembro no encontrado"}), 400
 
+
 @app.route('/members', methods=['POST'])
 def create_member():
     new_member_data = request.get_json()
-    
+
     if not new_member_data:
-         return jsonify({"msg": "Debes enviar datos en el body"}), 400
+        return jsonify({"msg": "Debes enviar datos en el body"}), 400
 
     member_created = jackson_family.add_member(new_member_data)
-    
+
     return jsonify(member_created), 200
+
 
 @app.route('/members/<int:member_id>', methods=['DELETE'])
 def delete_member(member_id):
     eliminated = jackson_family.delete_member(member_id)
-    
+
     if eliminated:
         return jsonify({"done": True}), 200
     else:
         return jsonify({"msg": "No se encontró el miembro para eliminar"}), 400
+
 
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,3 +1,4 @@
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
